77a2b6ba62dfc12f658286cd27e7f75f8bc06227
Kyle <kyletupling@hotmail.com>
2026-01-20 13:06:56 +0000

    Update to new solution

--- solutions/test.py
+++ solutions/test.py
@@ -1,2 +1,16 @@
-def func():
-    return None
+def LargestNumberInList(numList):
+    # If the list is empty, don't attempt to index it.
+    # Instead, just return 0.
+    if len(numList) == 0:
+        return 0
+
+    # Initialise largest number to the first in the list.
+    largestNumber = numList[0]
+
+    # Check our initial largest number against every number
+    # in the list, starting at index 1 (we already used index 0).
+    for numIdx in range(1, len(numList)):
+        if numList[numIdx] > largestNumber:
+            largestNumber = numList[numIdx]
+
+    return largestNumber
