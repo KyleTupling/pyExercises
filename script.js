f21696a6ca339557db6e9c0e3a0302a90aa7b390
Kyle Tupling <kyletupling@hotmail.com>
2026-01-20 12:27:48 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+/* ---------------------------------------------
+ Collapse Box Functionality
+--------------------------------------------- */
+const collapseBoxTitle   = document.querySelectorAll(".collapse-box-title");
+const collapseBoxContent = document.querySelectorAll(".collapse-box-content");
+const collapseBoxArrow   = document.querySelectorAll(".down-arrow");
+
+// Add event listeners to all collapse boxes
+for (let i = 0; i < collapseBoxTitle.length; i++) {
+    collapseBoxTitle[i].addEventListener("click", () => {
+        const isOpen = collapseBoxContent[i].style.maxHeight;
+
+        if (isOpen) {
+            collapseBoxContent[i].style.maxHeight  = null;
+            collapseBoxContent[i].style.paddingBottom = "0";
+            collapseBoxArrow[i].classList.remove("open");
+        } else {
+            collapseBoxContent[i].style.maxHeight = collapseBoxContent[i].scrollHeight + "px";
+            collapseBoxContent[i].style.paddingBottom = "12px";
+            collapseBoxArrow[i].classList.add("open");
+        }
+    });
+}
+
+fetch("solutions/test.py");
